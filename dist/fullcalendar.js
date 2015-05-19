55d21070186099c7e81986797806f2575d743764
Marc <marc@docorga.com>
2015-05-19 18:15:52 +0200

    Handle mousewheel on month view

--- dist/fullcalendar.js
+++ dist/fullcalendar.js
@@ -7999,6 +7999,19 @@ function Calendar_constructor(element, overrides) {
 			windowResizeProxy = debounce(windowResize, options.windowResizeDelay); // prevents rapid calls
 			$(window).resize(windowResizeProxy);
 		}
+
+		content.on('mousewheel', debounce(function(event) {
+			if(t.view.type === "month") {
+				var delta = event.originalEvent.wheelDelta;
+				if (delta < 0) {
+					prev();
+				}
+				if (delta > 0) {
+					next();
+				}
+			}
+
+		}, 300, true));
 	}
 	
 	
