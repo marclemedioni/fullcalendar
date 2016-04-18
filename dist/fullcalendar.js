725f1e8650f09cc25234dbbdac38535ec54a70a0
Marc <marc@docorga.com>
2016-04-18 11:20:06 +0200

    version 2.3.9

--- dist/fullcalendar.js
+++ dist/fullcalendar.js
@@ -1,7 +1,7 @@
 /*!
- * <%= meta.title %> v<%= meta.version %>
- * Docs & License: <%= meta.homepage %>
- * (c) <%= meta.copyright %>
+ * FullCalendar v2.3.9
+ * Docs & License: http://fullcalendar.io/
+ * (c) 2015 Adam Shaw
  */
 
 (function(factory) {
@@ -18,7 +18,7 @@
 
 ;;
 
-var fc = $.fullCalendar = { version: "<%= meta.version %>" };
+var fc = $.fullCalendar = { version: "2.3.9" };
 var fcViews = fc.views = {};
 
 
@@ -10883,4 +10883,3 @@ fcViews.agendaDay = {
 
 return fc; // export for Node/CommonJS
 });
-//@ sourceMappingURL=fullcalendar.js.map
--- dist/gcal.js
+++ dist/gcal.js
@@ -1,7 +1,7 @@
 /*!
- * <%= meta.title %> v<%= meta.version %> Google Calendar Plugin
- * Docs & License: <%= meta.homepage %>
- * (c) <%= meta.copyright %>
+ * FullCalendar v2.3.9 Google Calendar Plugin
+ * Docs & License: http://fullcalendar.io/
+ * (c) 2015 Adam Shaw
  */
  
 (function(factory) {
@@ -182,5 +182,3 @@ function injectQsComponent(url, component) {
 
 
 });
-
-//@ sourceMappingURL=gcal.js.map
